2d88da56118f988035445312af8f745777f4c345
ReenKhan <shireenk154@gmail.com>
2021-07-12 01:40:11 +0400

    first commit

--- server.js
+++ server.js
@@ -19,3 +19,15 @@
 // server.listen(port,()=>{
 //     console.log("server running on", port)
 // });
+
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+});
