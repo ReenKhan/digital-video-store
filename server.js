69e3fa4ce6df9c484aafad1773ca14e10ae30630
ReenKhan <shireenk154@gmail.com>
2021-07-11 22:49:23 +0400

    first commit

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-// const jsonServer=require('json-server');
-// const server=jsonServer.create();
-// const express=require('express');
-
-// const cors=require('cors');
-// const router =jsonServer.router('./db.json');
-// const middlewares= jsonServer.defaults();
-
-// const PORT =process.env.PORT || 3000;
-
-// server.use(cors());
-// server.use(express.static('build'));
-// server.use(middlewares);
-// server.use('/api',router);
-
-// server.listen(PORT, ()=>{
-//     console.log('JSON Server is running on', PORT)
-// });
-
+const jsonServer=require('json-server');
+const server=jsonServer.create();
 const express=require('express');
-const app=express();
-const importData=require("./db.json");
+
+const cors=require('cors');
+const router =jsonServer.router('./db.json');
+const middlewares= jsonServer.defaults();
 
 const PORT =process.env.PORT || 3000;
 
-    app.get('/', (req,res)=>{
-        req.send(importData);
-    });
+server.use(cors());
+server.use(express.static('build'));
+server.use(middlewares);
+server.use(router);
 
-app.listen(PORT, ()=>{
-     console.log(`JSON Server is running on port http://localhost:${PORT}` )
+server.listen(PORT, ()=>{
+    console.log('JSON Server is running on', PORT)
 });
-
-
-
-
