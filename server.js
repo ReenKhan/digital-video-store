1575c342484dd4f3df3fd3136ece86cee227ec00
ReenKhan <shireenk154@gmail.com>
2021-07-12 01:02:20 +0400

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+// const jsonServer = require('json-server');
+// const app = jsonServer.create();
+// const path = require('path');
+// const express = require('express');
+// const cors=require('cors');
+// const middlewares = jsonServer.defaults();
+// const router = jsonServer.router('db.json');
+
+// const port = process.env.PORT || 4000;
+
+// app.use('/db', middlewares, router);
+// app.use(cors());
+// app.use(express.static('build'));
+
+// // app.get('/*', function (req, res) {
+// //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// // });
+
+// server.listen(port,()=>{
+//     console.log("server running on", port)
+// });
 const jsonServer = require('json-server');
-const app = jsonServer.create();
-const path = require('path');
-const express = require('express');
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const port = process.env.PORT || 3001;
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
 
-app.use('/db', middlewares, router);
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+server.use(middlewares);
+server.use(router);
 
 server.listen(port);
