5d9d55413b9c9c290ecd50438cb98e8f68f0173d
ReenKhan <shireenk154@gmail.com>
2021-07-11 22:56:43 +0400

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const jsonServer=require('json-server');
-const server=jsonServer.create();
-const express=require('express');
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
 
-const cors=require('cors');
-const router =jsonServer.router('./db.json');
-const middlewares= jsonServer.defaults();
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-const PORT =process.env.PORT || 3000;
-
-server.use(cors());
-server.use(express.static('build'));
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, ()=>{
-    console.log('JSON Server is running on', PORT)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+server.listen(port);
